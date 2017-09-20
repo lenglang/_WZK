619eb63ef0cd17a595c60ef9c757ea23c696dd18
王则昆 <364127728@qq.com>
2017-09-20 15:22:13 +0800

    批量修改文件名

---
+++ 24.物体漂浮效果/Floating.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+namespace WZK
+{
+    /// <summary>
+    /// 浮动效果
+    /// </summary>
+    public class Floating : MonoBehaviour
+    {
+        [Header("x轴移动距离")]
+        public float _xMoveDis = 0;
+        [Header("x轴移动弧度")]
+        public float _xMoveRadian = 0f;
+        private float _xRadian = 0;//弧度增量
+        [Header("y轴移动距离")]
+        public float _yMoveDis = 0;
+        [Header("x轴移动弧度")]
+        public float _yMoveRadian = 0f;
+        private float _yRadian = 0;//弧度增量
+        [Header("z轴移动距离")]
+        public float _zMoveDis = 0;
+        [Header("z轴移动弧度")]
+        public float _zMoveRadian = 0f;
+        private float _zRadian = 0;//弧度增量
+        private Vector3 _originTransform;//原点
+        void Start()
+        {
+            _originTransform = transform.position;
+        }
+        private void FixedUpdate()
+        {
+            _xRadian += _xMoveRadian;
+            _yRadian += _yMoveRadian;
+            _zRadian += _zMoveRadian;
+            transform.position = _originTransform + new Vector3(Mathf.Cos(_xRadian) * _xMoveDis, Mathf.Cos(_yRadian) * _yMoveDis, Mathf.Cos(_zRadian) * _zMoveDis);
+        }
+    }
+}
---
+++ 25.批量修改文件名/Editor/Rename.cs
@@ -0,0 +1,53 @@
+﻿
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+namespace WZK
+{
+    public class Rename : Editor
+    {
+        private static string _folderName = "";//文件夹名
+        private static string _assetPath = "";//文件项目路径
+        private static string _folderPath = "";//文件电脑路径
+        [MenuItem("WZK/更改名字/将文件夹下文件名含prefab的替换成文件夹名")]
+        public static void Rename1()
+        {
+            FileInfo[] files = GetFiles();
+            if (files == null || files.Length == 0) return;
+            string oldpath = "";
+            string newpath = "";
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].Name.EndsWith(".prefab") == false) continue;
+                oldpath = "";
+                newpath = "";
+                #region 因需要替换的名正好是后缀，所以才这么麻烦去处理
+                oldpath = files[i].FullName;
+                newpath = oldpath.Replace(files[i].Extension, "");
+                newpath = newpath.Replace("prefab", _folderName);
+                newpath += files[i].Extension;
+                #endregion
+                if (System.IO.File.Exists(oldpath))
+                {
+                    Debug.Log(newpath);
+                    System.IO.File.Move(oldpath, newpath);
+                }
+            }
+            AssetDatabase.Refresh();
+        }
+        /// <summary>
+        /// 获取文件夹里文件
+        /// </summary>
+        public static FileInfo[] GetFiles()
+        {
+            Object obj = Selection.activeObject;
+            if (obj==null||obj.GetType().ToString() != "UnityEditor.DefaultAsset") return null;
+            _assetPath = AssetDatabase.GetAssetPath(obj);
+            _folderName = _assetPath.Substring(_assetPath.LastIndexOf("/") + 1);
+            _folderPath = Application.dataPath.Substring(0, Application.dataPath.LastIndexOf("/") + 1) + _assetPath;
+            DirectoryInfo direction = new DirectoryInfo(_folderPath);
+            FileInfo[] files = direction.GetFiles("*", SearchOption.AllDirectories);
+            return files;
+        }
+    }
+}
