e6f21a63a169d27daea6d284a68442e67917e118
王则昆 <364127728@qq.com>
2017-10-11 16:25:35 +0800

    场景资源管理代码优化

--- 03.场景资源管理/SceneResources.cs
+++ 03.场景资源管理/SceneResources.cs
@@ -26,18 +26,6 @@ namespace WZK
         /// <returns></returns>
         public T Load<T>(string name) where T : Object
         {
-            if (name.Contains("/") == false)
-            {
-                string str;
-                int count = _resourcesConfig._objectList.Count;
-                for (int i = 0; i < count; i++)
-                {
-                    str = _resourcesConfig._objectList[i]._assetPath;
-                    str = str.Substring(str.LastIndexOf("/") + 1);
-                    if (name.Contains(".") == false) str = str.Substring(0, str.IndexOf("."));
-                    if (str == name) return (T)_resourcesConfig._objectList[i]._object;
-                }
-            }
             return (T)_resourcesConfig._objectList.Find(n => n._assetPath.Contains(name))._object;
         }
     }
